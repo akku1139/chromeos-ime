27f7cf75fe458e973490e5a8616273610d890685
-akku- <151517253+akku1139@users.noreply.github.com>
2025-10-07 10:53:08 +0900

    init ime

---
+++ src/background/index.js
@@ -0,0 +1,19 @@
+// https://developer.chrome.com/docs/extensions/reference/api/input/ime?hl=ja#examples
+
+let context_id = -1;
+
+chrome.input.ime.onFocus.addListener(function(context) {
+  context_id = context.contextID;
+});
+
+chrome.input.ime.onKeyEvent.addListener(
+  function(engineID, keyData) {
+    if (keyData.type == "keydown" && keyData.key.match(/^[a-z]$/)) {
+      chrome.input.ime.commitText({"contextID": context_id,
+                                    "text": keyData.key.toUpperCase()});
+      return true;
+    } else {
+      return false;
+    }
+  }
+);
